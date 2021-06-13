1b662585f7daf6e9e6df136fa5974fe8b857a965
liurenpeng <liurenpeng@kuaishou.com>
2021-06-13 18:57:48 +0800

    init

---
+++ src/main/kotlin/com/agile4j/demo/Demo.kt
@@ -0,0 +1,9 @@
+package com.agile4j.demo
+
+/**
+ * @author liurenpeng <liurenpeng@kuaishou.com>
+ * Created on 2021-06-13
+ */
+fun main() {
+    println("demo")
+}
---
+++ src/test/kotlin/com/agile4j/demo/Test.kt
@@ -0,0 +1,16 @@
+package com.agile4j.demo
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * @author liurenpeng <liurenpeng@kuaishou.com>
+ * Created on 2021-06-13
+ */
+class Test {
+
+    @Test
+    fun test() {
+        Assert.assertTrue(true)
+    }
+}
