c463b6315281f4276a160da87728701ca66fafdf
liurenpeng <liurenpeng@kuaishou.com>
2021-06-15 10:28:45 +0800

    bio-chatroom-init

---
+++ src/main/java/com/agile4j/demo/io/bio/chatroom/client/ChatClient.java
@@ -0,0 +1,8 @@
+package com.agile4j.demo.io.bio.chatroom.client;
+
+/**
+ * @author liurenpeng <liurenpeng@kuaishou.com>
+ * Created on 2021-06-14
+ */
+public class ChatClient {
+}
---
+++ src/main/java/com/agile4j/demo/io/bio/chatroom/server/CharServer.java
@@ -0,0 +1,8 @@
+package com.agile4j.demo.io.bio.chatroom.server;
+
+/**
+ * @author liurenpeng <liurenpeng@kuaishou.com>
+ * Created on 2021-06-14
+ */
+public class CharServer {
+}
